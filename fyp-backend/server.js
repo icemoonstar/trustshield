bb17e2a2cfdbd4b8e3e38ee526b31703a8f1dc51
Wong Si Jun <wongsjun.24@gmail.com>
2025-07-21 01:31:09 +0800

    000000

--- fyp-backend/server.js
+++ fyp-backend/server.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
+require('dotenv').config();
+
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 
 const app = express();
 app.set('trust proxy', true); 

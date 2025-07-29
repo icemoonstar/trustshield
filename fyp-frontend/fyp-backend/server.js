db3f44e94e93fc816fb5879a88a4c5969e49a94c
Wong Si Jun <wongsjun.24@gmail.com>
2025-07-30 00:20:57 +0800

    45

--- fyp-frontend/fyp-backend/server.js
+++ fyp-frontend/fyp-backend/server.js
@@ -61,9 +61,3 @@ app.get('/get-ip', (req, res) => {
 app.listen(PORT, () => {
   console.log(`✅ FYP Server is running on http://localhost:${PORT}`);
 });
-
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`✅ FYP Server is running on http://localhost:${PORT}`);
-});
